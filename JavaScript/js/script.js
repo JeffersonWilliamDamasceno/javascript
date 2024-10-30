5806471910af7e613e8313fca0038c0fdee6f1c0
Jefferson William Damasceno Silva <jwdamasceno24@outlook.com>
2024-10-29 21:19:31 -0300

    Aula 29/10/2024

---
+++ JavaScript/js/script.js
@@ -0,0 +1,35 @@
+var nome = prompt(" Seu nome é?")
+var idade = prompt("Sua idade é?")
+var curso = prompt("Seu curso é?")
+var profissao = prompt("Sua profissão é?")
+
+alert(`Seu nome é: ${nome}
+Sua idade é: ${idade}
+Seu curso é: ${curso}
+Sua profissão é: ${profissao}`)
+
+
+
+
+//var vagaEstacionamento = "carro branco"
+
+//console.log(vagaEstacionamento)
+
+//var vagaEstacionamento = "carro vermelho"
+
+//console.log(vagaEstacionamento)
+
+//var nome = prompt("Digite seu nome")
+
+//alert("Olá " + nome + ", seja bem-vindo!")
+
+// alert(`Olá ${nome} , seja bem-vindo!`)
+
+//var primeiroValor = Number(prompt("Digite o numero:")) 
+//var segundoValor = Number(prompt("Digite outro numero:")) 
+//var resultadoSoma = primeiroValor + segundoValor 
+
+//console.log(primeiroValor)
+//console.log(segundoValor)
+
+//alert(`O resultado da soma = ${resultadoSoma}`);
